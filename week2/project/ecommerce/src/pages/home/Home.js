392953a3942a21ace7ed609cfd4eda0e99236b58
zehra <zehrakocairi@gmail.com>
2023-12-19 10:11:40 +0000

    use api to fetch data

--- week2/project/ecommerce/src/pages/home/Home.js
+++ week2/project/ecommerce/src/pages/home/Home.js
@@ -1,15 +1,39 @@
 import Categories from "../../components/category/Categories";
 import Products from "../../components/product/Products";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import categoriesData from "../../fake-data/all-categories.js";
 import productsData from "../../fake-data/all-products.js";
 
 const Home = () => {
   const [filteredProducts, setFilteredProducts] = useState(productsData);
-  const [categories] = useState(categoriesData.map((category) => category.replace("FAKE:", "").trim()));
+  const [categories, setCategories] = useState(categoriesData.map((category) => category.replace("FAKE:", "").trim()));
 
-  function onCategoryChanged(newCategory) {
-    setFilteredProducts(productsData.filter((product) => product.category === newCategory));
+  const fetchCategories = async function () {
+    const response = await fetch("https://fakestoreapi.com/products/categories");
+    const categories = await response.json();
+    setCategories(categories);
+  };
+
+  const fetchProducts = async function () {
+    const response = await fetch("https://fakestoreapi.com/products");
+    const products = await response.json();
+    setFilteredProducts(products);
+  };
+
+  const fetchProductsByCategory = async function (category) {
+    const response = await fetch(`https://fakestoreapi.com/products/category/${category}`);
+    const products = await response.json();
+
+    setFilteredProducts(products);
+  };
+
+  useEffect(() => {
+    fetchCategories();
+    fetchProducts();
+  }, []);
+
+  async function onCategoryChanged(newCategory) {
+    await fetchProductsByCategory(newCategory);
   }
 
   return (
